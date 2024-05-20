099554fd740c2f24ba58c1ce1f81a0d2f30ad12a
ErtelDavid21 <145244946+ErtelDavid21@users.noreply.github.com>
2024-05-20 08:22:26 -0600

    Reorganization of files to correct folders

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,14 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to your diary, don't forget to hide this from prying eyes.");
+        Boolean writing = true;
+        Journal diary = new Journal();
+
+        while (writing != false)
+        {
+            diary.journalLoad(writing);
+        }
     }
 }
